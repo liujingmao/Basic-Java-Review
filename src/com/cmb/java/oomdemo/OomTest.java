6f4f3f9a72221aac7fe29e0f30519505073cab3b
liujingmao <160143836@qq.com>
2022-12-13 08:37:22 +0800

    Add comment for How To Analize Oom

--- src/com/cmb/java/oomdemo/OomTest.java
+++ src/com/cmb/java/oomdemo/OomTest.java
@@ -4,6 +4,23 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
+
+/**
+ * Comman Oom Demo Test
+ * Learn How to Analyze Oom of Program
+ * 1. Program
+ * 2. Use Default VM Parameter,will spend long time to
+ *    exec it,so set vm parameter smaller,as flowing
+ *    -ea -Xmx5m -XX:+HeapDumpOnOutOfMemoryError
+ *    -XX:HeapDumpPath=/Users/liujingmao/Downloads/JavaBasic/untitled/src/com/cmb/java/heap.hprof
+ * 3. start the program
+ * 4. get java program's proesss ID using 'jps' this command
+ * 5. generate dump file using the comman as flowing
+ *     jmap -dump:format=b,file=./thisdump.hprof 61071(pid)
+ * 6. Analize the dump filw with Mat Tool by import thisdump.hprof
+ * 7. Detail Analysis
+ */
+
 public class OomTest {
     public static void main(String[] args) throws InterruptedException {
 
