490dcf3d9e7dce728b8e6aa4a649d4d9f8aeecf0
liujingmao <160143836@qq.com>
2022-12-17 18:07:37 +0800

    add HashSetDemo

---
+++ src/com/cmb/java/hashset/HashSetDemo.java
@@ -0,0 +1,34 @@
+package com.cmb.java.hashset;
+
+import java.util.HashSet;
+
+/**
+ * 1. implement function base on hashMap, repeat elements are not allowed
+ * 2. null value
+ * 3. unsafe thread
+ * 4. implement set this Interface
+ */
+
+public class HashSetDemo {
+    public static void main(String[] args) {
+
+        HashSet<String> strHashset = new HashSet<>();
+        strHashset.add("Helloword");
+        strHashset.add("A");
+        strHashset.add("B");
+        strHashset.add("C");
+        strHashset.add("A");
+        System.out.println(strHashset.size());
+        for (String str:strHashset
+             ) {
+            System.out.println(str);
+        }
+        System.out.println(strHashset);
+        System.out.println(strHashset.contains("D"));
+        strHashset.clear();
+        System.out.println(strHashset);
+
+
+
+    }
+}
