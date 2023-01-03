d44d899790f1394eccc8cfb60f6b2c3bf01e2483
liujingmao <160143836@qq.com>
2023-01-03 08:44:35 +0800

    First Lambda Test

---
+++ src/com/cmb/java/lambda/App.java
@@ -0,0 +1,29 @@
+package com.cmb.java.lambda;
+
+/**
+ *
+ */
+
+public class App {
+    public static void main(String[] args) {
+
+        Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("这是匿名内部类输出");
+            }
+        };
+
+        Runnable runnable1 = () -> System.out.println("这是lambda表达式输出");
+
+        runnable.run();
+
+        System.out.println("******************* Parting Line ********************");
+
+        runnable1.run();
+
+
+
+
+    }
+}
