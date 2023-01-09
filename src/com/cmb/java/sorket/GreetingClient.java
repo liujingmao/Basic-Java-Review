11088d46c583872d9103e04107d8b21674b3d69e
liujingmao <160143836@qq.com>
2023-01-09 08:48:27 +0800

    Java Soket Programer

---
+++ src/com/cmb/java/sorket/GreetingClient.java
@@ -0,0 +1,29 @@
+package com.cmb.java.sorket;
+
+
+import java.io.*;
+import java.net.Socket;
+
+public class GreetingClient {
+    public static void main(String[] args) {
+
+        String serverName = args[0];
+        int port = Integer.parseInt(args[1]);
+
+        try{
+            System.out.println("Connect To Host's Server:"+serverName+",端口："+port);
+            Socket client = new Socket(serverName,port);
+            System.out.println("Remote Hostname:"+client.getRemoteSocketAddress());
+            OutputStream outToServer = client.getOutputStream();
+            DataOutputStream out = new DataOutputStream(outToServer);
+
+            out.writeUTF("Hello From"+client.getLocalSocketAddress());
+            InputStream inFromServer = client.getInputStream();
+            DataInputStream in = new DataInputStream(inFromServer);
+            System.out.println("Server Respon:"+in.readUTF());
+            client.close();
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+}
