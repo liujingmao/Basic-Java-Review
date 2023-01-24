695d671d821be22382898fa2798573d496b76267
liujingmao <160143836@qq.com>
2023-01-24 18:30:02 +0800

    Make Note For HelleWordTest Class

--- src/com/cmb/java/HelleWorldTest.java
+++ src/com/cmb/java/HelleWorldTest.java
@@ -3,5 +3,13 @@ package com.cmb.java;
 public class HelleWorldTest {
     public static void main(String[] args) {
         System.out.println("This is Hello World Test");
+
+        HelleWorldTest helleWorldTest = new HelleWorldTest();
+        String result = helleWorldTest.printString("Java Programmer!!!!!");
+        System.out.println("String Result:"+result);
+    }
+
+    public String printString(String str){
+        return str;
     }
 }
